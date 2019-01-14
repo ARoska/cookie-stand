e5f18bc008125c7f39f7f5e8847875a651a41fcb
Andrew Roska <andrew.l.roska@gmail.com>
2019-01-14 14:11:44 -0800

    Adds initial objects

---
+++ js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var openHours = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+var firstAndPine = {
+    name: '1st and Pine',
+    minCust: 23,
+    maxCust: 65,
+    avgPerCust: 6.3,
+
+};
+
+var seaTac = {
+    name: 'SeaTac Airport',
+    minCust: 3,
+    maxCust: 24,
+    avgPerCust: 1.2,
+    custPerHour: [],
+    salesPerHour: [],
+    footTraffic: function () {
+        for (i = 0; i < openHours.length; i++);
+    }
+};
+
+var seaCenter = {
+    name: 'Seattle Center',
+    minCust: 11,
+    maxCust: 38,
+    avgPerCust: 3.7,
+
+};
+
+var capHill = {
+    name: 'Capitol Hill',
+    minCust: 20,
+    maxCust: 38,
+    avgPerCust: 2.8,
+
+};
+
+var alki = {
+    name: 'Alki Beach'
+    minCust: 2,
+    maxCust: 16,
+    avgPerCust: 4.6,
+
+};
